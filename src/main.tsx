1e60f18c47b371267e94068020a6a1915e9faef2
Luis Mera <luismera86@gmail.com>
2023-01-14 01:14:57 -0300

    Configuración de rutas iniciales

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import './index.css'
 
 import { AppRouter } from './router'
+import { BrowserRouter } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <AppRouter />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <AppRouter />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+
+import { AuthRoutes } from '@/auth/routes'
 
 export const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <Routes>
+      <Route path='/auth/*' element={<AuthRoutes />} />
+      <Route path='/' element={<div>Inicio</div>} />
+    </Routes>
   )
 }
