a406763dd2b3bdc50335f4de82f89ff5fa236b26
Luis Mera <luismera86@gmail.com>
2023-01-14 01:22:19 -0300

    Configuración de rutas iniciales de auth

--- src/auth/routes/AuthRoutes.tsx
+++ src/auth/routes/AuthRoutes.tsx
@@ -1,9 +1,12 @@
+import { LoginPage, RegisterPage } from '../pages'
 import { Route, Routes } from 'react-router-dom'
 
 export const AuthRoutes = () => {
   return (
-    <Routes >
-      <Route path='/' element={<div>Inicio auth</div>} />
+    <Routes>
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/*' element={<LoginPage />} />
     </Routes>
   )
 }
