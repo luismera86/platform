da855cfc6be316d5500397186ac2697b4e95a48a
Luis Mera <luismera86@gmail.com>
2023-01-21 11:19:47 -0300

    dependencias y componentes con material ui quitados para comenzar a trabajar con tailwind

--- src/auth/layout/AuthLayout.tsx
+++ src/auth/layout/AuthLayout.tsx
@@ -1,4 +1,3 @@
-import { Box, Typography } from '@mui/material'
 
 interface Props {
   title: string
@@ -7,18 +6,5 @@ interface Props {
 
 export const AuthLayout = ({ title, children }: Props) => {
   return (
-    <Box
-      minHeight='100vh'
-      flexDirection='column'
-      display='flex'
-      justifyContent='center'
-      alignItems='center'
-      textAlign='center'
-    >
-      <Box border={3} borderColor='black' p={5} minWidth={{xs: 300, md: 500}}>
-        <Typography fontWeight={700}>{title}</Typography>
-        {children}
-      </Box>
-    </Box>
   )
 }
