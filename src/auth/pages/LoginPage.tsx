a63d9defcc0e57a1b53ef0d58f911bb2ea77a70a
Luis Mera <luismera86@gmail.com>
2023-01-22 23:44:09 -0300

    modificaciones al login

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,9 +1,14 @@
-
-
 export const LoginPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
-}
+    <div className="flex flex-col h-screen justify-center items-center">
+      <div className="bg-[#F6BD60] w-1/2 max-w-sm h-auto p-10 rounded shadow-md">
+        <form className="flex flex-col space-y-2">
+          <label className="text-xl text-[#84A59D]">E-mail</label>
+          <input className="shadow-md rounded text-xl p-2" type="email" name="mail" id="" />
+          <label className="text-xl text-[#84A59D]">Contraseña</label>
+          <input className="shadow-md rounded text-xl p-2" type="password" name="password" id="" />
+        </form>
+      </div>
+    </div>
+  );
+};
